f38af29f55fafc9d59bbf63891c7d429570d56dd
Jrorem <joerorem@gmail.com>
2023-04-22 10:32:04 -0500

    commit message

--- assets/script.js
+++ assets/script.js
@@ -7,40 +7,52 @@ var nameVenue = document.getElementById("venueName")
 var time = document.getElementById("timeDate")
 var url = document.getElementById("url")
 var SGUrl = "https://api.seatgeek.com/2/events?q=&client_id=MzMwMzM2MzJ8MTY4MTM1MTUyMi41MjQwMzgz&client_secret=505cbc8cb48518aae371b19ec2913f6a4da13d3b010e86d7561ed8bf208ec81c"
+
+
 // click button after entering artist in search bar  
- button.addEventListener("click", function displayConcert() {
-
+button.addEventListener("click", function displayConcert() {
  event.preventDefault()
+// function displayConcert(){
+// document.getElementById("default-search").onsubmit()
+
  fetch("https://api.seatgeek.com/2/events?q=" + artistValue.value + "&geoip=true&taxonomies.name=concert&range=250mi&client_id=MzMwMzM2MzJ8MTY4MTM1MTUyMi41MjQwMzgz&client_secret=505cbc8cb48518aae371b19ec2913f6a4da13d3b010e86d7561ed8bf208ec81c")  
-
-event.preventDefault()
- fetch("https://api.seatgeek.com/2/events?q=" + artistValue.value + "&client_id=MzMwMzM2MzJ8MTY4MTM1MTUyMi41MjQwMzgz&client_secret=505cbc8cb48518aae371b19ec2913f6a4da13d3b010e86d7561ed8bf208ec81c")  
-
 .then(function (response){
   return response.json()
 })
 .then(function (data) {
   console.log(data)
-  
-for(var i = 0; i < 2; i++){
-    // event = event[i]
+  if (!data){
+    fetch("https://binaryjazz.us/wp-json/genrenator/v1/story/"
+    , {
+       
+       
+     })
+       .then(function (response) {
+         return response.json();
+       })
+       .then(function (data) {
+         console.log(data);
+       });
+  }
+
+  for(var i = 0; i < 3; i++){
     var concertName = data["events"][i]["title"]
     var cityState = data["events"][i]["venue"]["display_location"]
-    
     var venueName = data["events"][i]["venue"]["name"]
-    var timeDate = data["events"][i]["datetime_utc"]
+    var timeDate = data["events"][i]["datetime_local"]
     var ticketUrl = data["events"][i]["url"]
-    title.innerHTML = "Concert name: " + concertName 
-    venue.innerHTML = "Location: " + cityState
-    nameVenue.innerHTML = "Venue name: " + venueName
-    time.innerHTML = "Date/Time :" + timeDate
-    url.innerHTML = "link to Seat Geek: " + ticketUrl
-}
+    $("#concertName" + i).html(concertName)
+    $("#cityState"+ i).html(cityState)
+    $("#venueName"+ i).html(venueName)
+    $("#timeDate"+ i).html(timeDate)
+    $("#url" + i).html(ticketUrl)
+  }
+
   displayConcert()
 })
-  
+// }
 })
-  
+  // use the search near me button
 nearButton.addEventListener("click", function displayConcert(){
   event.preventDefault()
   fetch("https://api.seatgeek.com/2/events?q=&geoip=true&taxonomies.name=concert&sort=datetime_utc.asc&client_id=MzMwMzM2MzJ8MTY4MTM1MTUyMi41MjQwMzgz&client_secret=505cbc8cb48518aae371b19ec2913f6a4da13d3b010e86d7561ed8bf208ec81c")
@@ -54,31 +66,24 @@ nearButton.addEventListener("click", function displayConcert(){
       var concertName = data["events"][i]["title"]
       var cityState = data["events"][i]["venue"]["display_location"]
       var venueName = data["events"][i]["venue"]["name"]
-      var timeDate = data["events"][i]["datetime_utc"]
+      var timeDate = data["events"][i]["datetime_local"]
       var ticketUrl = data["events"][i]["url"]
-      title.innerHTML = "Concert name: " + concertName 
-      venue.innerHTML = "Location: " + cityState
-      nameVenue.innerHTML = "Venue name: " + venueName
-      time.innerHTML = "Date/Time :" + timeDate
-      url.innerHTML = "link to Seat Geek: " + ticketUrl
+      // ticketUrl.href = data["events"][i]["url"]
+      $("#concertName" + i).html(concertName)
+      $("#cityState" + i).html(cityState)
+      $("#venueName" + i).html(venueName)
+      $("#timeDate" + i).html(timeDate)
+      // url.innerHTML = "link to Seat Geek: " + ticketUrl
+      $("#url" + i).html(ticketUrl)
   }
     displayConcert()
   })
 })
 
   
-// button.addEventListener("click", function(){
-  fetch("https://binaryjazz.us/wp-json/genrenator/v1/story/"
- , {
-    
-    
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+// nearButton.addEventListener("click", function(){
+
 
   // })
+
 
