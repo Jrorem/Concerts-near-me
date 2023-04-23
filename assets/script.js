1cede8ec9b191a697000b3fc310ad80026bf45d9
osayenko0315 <126353984+osayenko0315@users.noreply.github.com>
2023-04-23 18:46:30 -0500

    genrenator

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,7 @@ var SGUrl = "https://api.seatgeek.com/2/events?q=&client_id=MzMwMzM2MzJ8MTY4MTM1
 button.addEventListener("click", function displayConcert() {
 event.preventDefault()
 clearCearch()
+getSentence()
 // sentence()
  fetch("https://api.seatgeek.com/2/events?q=" + artistValue.value + "&geoip=true&taxonomies.name=concert&range=250mi&client_id=MzMwMzM2MzJ8MTY4MTM1MTUyMi41MjQwMzgz&client_secret=505cbc8cb48518aae371b19ec2913f6a4da13d3b010e86d7561ed8bf208ec81c")  
 .then(function (response){
@@ -51,7 +52,7 @@ clearCearch()
 nearButton.addEventListener("click", function displayConcert(){
   event.preventDefault()
   clearCearch()
-  // sentence()
+  getSentence()
   fetch("https://api.seatgeek.com/2/events?q=&geoip=true&taxonomies.name=concert&sort=datetime_utc.asc&client_id=MzMwMzM2MzJ8MTY4MTM1MTUyMi41MjQwMzgz&client_secret=505cbc8cb48518aae371b19ec2913f6a4da13d3b010e86d7561ed8bf208ec81c")
   .then(function(response){
     return response.json()
@@ -86,3 +87,17 @@ function clearCearch(){
   }
 }
  
+function getSentence(){
+  fetch("https://binaryjazz.us/wp-json/genrenator/v1/story/"
+    , {
+     })
+       .then(function (response) {
+         return response.json();
+       })
+       .then(function (data) {
+        var sentence = data
+       $("#sentence").html("Conversation starter: " + sentence)
+         console.log(data);
+         return
+       });
+      }
